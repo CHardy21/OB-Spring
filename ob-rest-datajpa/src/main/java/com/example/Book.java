2062089267dc4f50f0932204646e4d9742fa9ef1
CHardy <christianrh2000@hotmail.com>
2023-02-10 11:31:01 -0300

    ob-rest-datajpa

---
+++ ob-rest-datajpa/src/main/java/com/example/Book.java
@@ -0,0 +1,105 @@
+package com.example;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Book {
+
+	// atributos
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private Integer pages;
+    private Double price;
+    private LocalDate releaseDate;
+    private Boolean online;
+
+    // constructores
+    
+    public Book() {
+    	super();
+	}
+
+	public Book(Long id, String title, String author, Integer pages, Double price, LocalDate releaseDate,
+			Boolean online) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.price = price;
+		this.releaseDate = releaseDate;
+		this.online = online;
+	}
+	
+	// Getters y Setters
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Integer getPages() {
+		return pages;
+	}
+
+	public void setPages(Integer pages) {
+		this.pages = pages;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public LocalDate getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(LocalDate releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public Boolean getOnline() {
+		return online;
+	}
+
+	public void setOnline(Boolean online) {
+		this.online = online;
+	}
+    
+
+	
+	
+    
+}
