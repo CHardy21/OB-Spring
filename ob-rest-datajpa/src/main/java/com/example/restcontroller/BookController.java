65071ad0b5a3e217dcf13d9d6c14eb3a3ca8a294
CHardy <christianrh2000@hotmail.com>
2023-02-15 09:06:15 -0300

    swagger

--- ob-rest-datajpa/src/main/java/com/example/restcontroller/BookController.java
+++ ob-rest-datajpa/src/main/java/com/example/restcontroller/BookController.java
@@ -6,9 +6,11 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -62,6 +64,51 @@ public class BookController {
 	}
 	
 	// actualizar info de un book
+	@PutMapping("/api/books")
+	public ResponseEntity<Book> update(@RequestBody Book book) {
+		// verifico si me estan pasando un id
+		if(book.getId() == null) {
+			log.warn("log: trying update a non existent book");
+			System.out.println("trying update a non existent book");
+			
+			return ResponseEntity.badRequest().build();
+		} 
+		
+		if(!bookRepository.existsById(book.getId())) {
+			log.warn("log: trying update a non existent book");
+			System.out.println("trying update a non existent book");
+			
+			return ResponseEntity.notFound().build();
+		} 
+
+		Book response = bookRepository.save(book);
+		return ResponseEntity.ok(response);
+		
+	}
+	
+	// borrar un book
+	@DeleteMapping("/api/books/{id}")
+	public ResponseEntity<Book> deleteById(@PathVariable Long id){
+			
+		if(!bookRepository.existsById(id)) {
+			log.warn("log: trying to delete a non existent book");
+			System.out.println("trying to delete a non existent book");
+				
+			return ResponseEntity.notFound().build();
+		} 
+			
+		bookRepository.deleteById(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	// borrar todos los books
+	@DeleteMapping("/api/books")
+	public ResponseEntity<Book> deleteAll(){
+		log.info("REST Request for DELETE All books");
+		bookRepository.deleteAll();
+		return ResponseEntity.noContent().build();
+	}
+	
 	
 	
 }
